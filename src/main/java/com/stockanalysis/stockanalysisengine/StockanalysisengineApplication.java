bcce90d156ba8458e52937af7c15ee5374a183d2
castor65 <castor65@126.com>
2021-12-31 00:30:28 +0800

    Initial commit

---
+++ src/main/java/com/stockanalysis/stockanalysisengine/StockanalysisengineApplication.java
@@ -0,0 +1,13 @@
+package com.stockanalysis.stockanalysisengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockanalysisengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockanalysisengineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stockanalysis/stockanalysisengine/StockanalysisengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockanalysis.stockanalysisengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockanalysisengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
