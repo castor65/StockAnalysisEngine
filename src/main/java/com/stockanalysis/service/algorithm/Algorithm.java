f0a0cbb7693047bb7ba2ac69b13af27fb4468e3b
C5313358 <castor65@126.com>
2022-01-04 18:18:02 +0800

    refactor

--- src/main/java/com/stockanalysis/service/algorithm/Algorithm.java
+++ src/main/java/com/stockanalysis/service/algorithm/Algorithm.java
@@ -12,23 +12,14 @@ import java.util.List;
 
 public class Algorithm {
 
-    public boolean pickUp(DataFrame singelStockGloble, Date calculateDate) {
+    public boolean pickUp(DataFrame singelStock, Date calculateDate) {
         boolean flag = true;
-        DataFrame singleStock = singelStockGloble.select(new DataFrame.Predicate<Object>() {
-            @SneakyThrows
-            @Override
-            public Boolean apply(List<Object> row) {
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                Date date = (Date) row.get(0);
-                return date.equals(calculateDate);
-            }
-        });
         try {
             Method[] methods = this.getClass().getDeclaredMethods();
             for (Method method : methods) {
                 if (method.getName().indexOf("condition") == 0) {
                     Boolean ifPassConditon = null;
-                    ifPassConditon = (Boolean) method.invoke(this, singleStock);
+                    ifPassConditon = (Boolean) method.invoke(this, singelStock,calculateDate);
                     if (!ifPassConditon) {
                         flag = false;
                         break;
@@ -41,10 +32,20 @@ public class Algorithm {
         }
         return flag;
     }
-    private boolean condition1(DataFrame singleStock) {
+    private boolean condition1(DataFrame singelStock,Date calculateDate) {
         Boolean flag = false;
-        if (singleStock.col("涨跌幅").size() > 0) {
-            if (Float.parseFloat(String.valueOf(singleStock.col("涨跌幅").get(0))) > 9.9) {
+        DataFrame singleStockInCondition = singelStock.select(new DataFrame.Predicate<Object>() {
+            @SneakyThrows
+            @Override
+            public Boolean apply(List<Object> row) {
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = (Date) row.get(0);
+                return date.equals(calculateDate);
+            }
+        });
+
+        if (singleStockInCondition.col("涨跌幅").size() > 0) {
+            if (Float.parseFloat(String.valueOf(singleStockInCondition.col("涨跌幅").get(0))) > 9.9) {
                 flag = true;
             }
         }
